63757679f92aa5d6c09b32c83ed086452e3b9f4b
Marineford2410 <marconava2410@gmail.com>
2019-05-24 16:31:51 -0500

    Se agrego el navbar rosa

---
+++ public/javascripts/main.js
@@ -0,0 +1,12 @@
+// Floating Actiion Buttom
+const elemsBtns = document.querySelectorAll(".fixed-action-btn");
+const floatingBtn = M.FloatingActionButton.init(elemsBtns, {
+    direction: "left",
+    hoverEnabled: false
+});
+
+// Navbar
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elemsDropdown, {
+    coverTrigger: false
+});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sistemas de Información' });
 });
 router.get('/conceptos', function(req, res, next) {
   res.render('conceptos', { title: 'Express' });
