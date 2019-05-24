c0530bddbaa4882b9f96cd98f0dd24c9f3ef8d60
LuisGC01 <luis_garciacervantes@yahoo.com.mx>
2019-05-23 21:43:09 -0500

    genere los .hbs de la pagina de conceptos y agregue etiquetas a con el la liga a las vistas de cada concepto

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,19 @@ router.get('/', function(req, res, next) {
 router.get('/conceptos', function(req, res, next) {
   res.render('conceptos', { title: 'Express' });
 });
+router.get('/Dato', function(req, res, next) {
+  res.render('Dato', { title: 'Express' });
+});
+
+router.get('/Informacion', function(req, res, next) {
+  res.render('Informacion', { title: 'Express' });
+});
+
+router.get('/SistemasDeInformacion', function(req, res, next) {
+  res.render('SistemasDeInformacion', { title: 'Express' });
+});
+
+
 
 
 module.exports = router;
